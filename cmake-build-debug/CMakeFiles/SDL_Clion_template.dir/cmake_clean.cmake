85acb421c4a49f7ccbafc41f4fc5846bc36bfd01
Justin Macklin <Jmacklin308>
2021-10-14 19:46:23 -0500

    update cmake list

---
+++ cmake-build-debug/CMakeFiles/SDL_Clion_template.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL_Clion_template.dir/src/main.cpp.obj"
+  "SDL_Clion_template.exe"
+  "SDL_Clion_template.exe.manifest"
+  "SDL_Clion_template.pdb"
+  "libSDL_Clion_template.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL_Clion_template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
